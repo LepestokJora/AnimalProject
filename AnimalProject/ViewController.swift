ca8855cf664a3e614fb7ccac6ee878d06db02811
Artem Peshkov <pewkoff@gmail.com>
2022-12-11 22:40:55 +0300

    Initial Commit

---
+++ AnimalProject/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  AnimalProject
+//
+//  Created by Lepestok Jora on 11.12.2022.
+//
+
+import UIKit
+import SpringAnimation
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var animalVC: UIView!
+    
+    @IBOutlet weak var parametersTF: UITextField!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    /*Создайте проект и интегрируйте в него фреймворк Spring при помощи SPM.
+     
+     Должна отображаться информация о текущей анимации: название и параметры анимации.
+     Запуск анимации должен происходить по нажатию на кнопку. При этом на самой кнопке должно отображаться название следующей анимации.
+     Анимации должны сменяться бесконечно в случайном порядке со случайными значениями параметров*/
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
