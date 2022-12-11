a7cc159c5b5189be2ce101d04d0633b67b29b44d
Artem Peshkov <pewkoff@gmail.com>
2022-12-11 23:26:41 +0300

    test animation

--- AnimalProject/ViewController.swift
+++ AnimalProject/ViewController.swift
@@ -11,13 +11,19 @@ import SpringAnimation
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var animalVC: UIView!
     
     @IBOutlet weak var parametersTF: UITextField!
     
     @IBOutlet weak var startButton: UIButton!
     
-    /*Создайте проект и интегрируйте в него фреймворк Spring при помощи SPM.
+    @IBOutlet weak var sprinView: SpringView!
+    private var animationStarted = false
+    
+    
+    
+    
+    
+    /*1.оздайте проект и интегрируйте в него фреймворк Spring при помощи SPM.
      
      Должна отображаться информация о текущей анимации: название и параметры анимации.
      Запуск анимации должен происходить по нажатию на кнопку. При этом на самой кнопке должно отображаться название следующей анимации.
@@ -28,8 +34,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        parametersTF.layer.cornerRadius = 50
+        startButton.layer.cornerRadius = 50
     }
 
+    
+    @IBAction func btnAction(_ sender: Any) {
+        
+        sprinView.animation = "squeezeDown"
+        sprinView.animate()
+
+        
+        
+        
+    }
+    
+    
+    
+    
 
 }
 
